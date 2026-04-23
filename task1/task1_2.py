68648978f301c278681ba344b7a811168c4c43eb
Паршенков Вячеслав Валерьевич <vycheslav79@gmail.com>
2026-04-23 14:16:48 +0300

    complexity for all tasks

--- task1/task1_2.py
+++ task1/task1_2.py
@@ -20,55 +20,55 @@ task: 1.1
 name: delete node from linked list method
 time: O(n)
 memory: O(1)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.2
 name: option to delete all occurse of val in linked list
 time: O(n)
 memory: O(1)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.3
 name: clean linked list method
 time: O(1)
 memory: O(1)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.4
 name: method that searches linked list and returns list of nodes
 time: O(n)
 memory: O(n)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.5
 name: len method
 time: O(n)
 memory: O(1)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.6
 name: insert new node into linked list
 time: O(1)
 memory: O(1)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.7
 name: tests
 time: O(n)
 memory: O(n)
-reflection: ...
+рефлексия: ...
 
 
 task: 1.8
 name: function for creation new linked list where l[n] = l1[n] + l2[n]
 time: O(n)
 memory: O(n)
-reflection: ...
+рефлексия: ...
 """
 
