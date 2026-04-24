921fb43fff8db29e7e998fb75843cf3634ed0dde
Паршенков Вячеслав Валерьевич <vycheslav79@gmail.com>
2026-04-24 13:46:38 +0300

    core methods implemented

--- task2/task2.py
+++ task2/task2.py
@@ -20,22 +20,77 @@ class LinkedList2:
         self.tail = item
 
     def find(self, val):
-        return None # здесь будет ваш код
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            if node.next == self.head:
+                return None
+            node = node.next
+        return None
 
     def find_all(self, val):
-        return [] # здесь будет ваш код
+        found_nodes = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                found_nodes.append(node)
+            if node.next == self.head:
+                return found_nodes
+            node = node.next
+        return found_nodes
 
     def delete(self, val, all=False):
-        pass # здесь будет ваш код
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                if node.prev is None or node.prev == self.tail:
+                    self.head = node.next
+                if node.next is None or node.next == self.head:
+                    self.tail = node.prev
+                if node.next is not None:
+                    node.next.prev = node.prev
+                if node.prev is not None:
+                    node.prev.next = node.next
+                if not all:
+                    return
+            node = node.next
 
     def clean(self):
-        pass # здесь будет ваш код
+        self.head = None
+        self.tail = None
 
     def len(self):
-        return 0 # здесь будет ваш код
+        node = self.head
+        i = 0
+        while node is not None:
+            i += 1
+            if node.next == self.head:
+                return i
+            node = node.next
+        return i
 
     def insert(self, afterNode, newNode):
-        pass # здесь будет ваш код
+        if afterNode is None:
+            self.add_in_head(newNode)
+            return
+        if self.head is None:
+            raise("afterNode should be None, when inserting in empty list")
+        if afterNode == self.tail:
+            self.add_in_tail(newNode)
+            return
+        newNode.next = afterNode.next
+        newNode.prev = afterNode
+        afterNode.next.prev = newNode
+        afterNode.next = newNode
 
     def add_in_head(self, newNode):
-        pass # здесь будет ваш код
+        if self.head is None:
+            newNode.prev = None
+            newNode.next = None
+            self.tail = newNode
+        else:
+            newNode.prev = None
+            newNode.next = self.head
+            self.head.prev = newNode
+        self.head = newNode
