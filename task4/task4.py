aa6d6d6a4ed4149c80eace1c93d3365511b356ab
Паршенков Вячеслав Валерьевич <vycheslav79@gmail.com>
2026-05-12 13:36:22 +0300

    working with tail

---
+++ task4/task4.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def peek(self):
+        if self.size() == 0:
+            return None
+        return self.stack[-1]
+
---
+++ task4/task4_3.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.mark.parametrize("values",[
+    [1, 2, 3, 4],
+    [1, 2, 3],
+])
+def test_peek():
+    pass
+
