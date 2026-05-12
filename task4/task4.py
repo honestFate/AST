2283d4a165789ae15cdc0794880101b644fbe56e
Паршенков Вячеслав Валерьевич <vycheslav79@gmail.com>
2026-05-12 13:55:50 +0300

    working with head

--- task4/task4.py
+++ task4/task4.py
@@ -8,13 +8,13 @@ class Stack:
     def pop(self):
         if self.size() == 0:
             return None
-        return self.stack.pop()
+        return self.stack.pop(0)
 
     def push(self, value):
-        self.stack.append(value)
+        self.stack.insert(0, value)
 
     def peek(self):
         if self.size() == 0:
             return None
-        return self.stack[-1]
+        return self.stack[0]
 
