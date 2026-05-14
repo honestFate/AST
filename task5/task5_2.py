bd778e0313b9a998ee4ff8855b8e86352ca5c9a4
Паршенков Вячеслав Валерьевич <vycheslav79@gmail.com>
2026-05-14 13:21:47 +0300

    task5 fix

--- task5/task5_2.py
+++ task5/task5_2.py
@@ -17,11 +17,13 @@ class CyclePointer:
     def get(self):
         return self.pointer
 
+
 class StaticArrayQueue:
     def __init__(self):
         self.queue = self._make_array(QUEQUE_CAPACITY)
         self.start = CyclePointer(QUEQUE_CAPACITY)
         self.end = CyclePointer(QUEQUE_CAPACITY)
+        self.len = 0
 
     def _make_array(self, new_capacity):
         return (new_capacity * ctypes.py_object)()
@@ -30,18 +32,20 @@ class StaticArrayQueue:
         if self.size() == QUEQUE_CAPACITY:
             raise OverflowError("QUEQUE_CAPACITY of {QUEQUE_CAPACITY} is full, " \
             "unable to add new items")
-        self.end.shift()
         self.queue[self.end] = item
+        self.end.shift()
+        self.len += 1
 
     def dequeue(self):
         if self.size() == 0:
             return None
         temp = self.queue[self.start.get()]
         self.start.shift()
+        self.len -= 1
         return temp
 
     def size(self):
-        return QUEQUE_CAPACITY - self.start.get() + self.end.get() if self.start.get() > self.end.get() else self.end.get() - self.start.get()
+        return self.len
 
 
 class StackQueue:
@@ -50,38 +54,32 @@ class StackQueue:
         self.remove_queue = Stack()
         self.mode = APPEND_MODE
 
-    def _change_mode(self, new_mode):
-        if self.mode == new_mode:
-            return
-        if not self.append_queue.size() and not self.remove_queue.size():
-            return
-        self.mode = new_mode
-        if new_mode == APPEND_MODE:
-            q1, q2 = self.remove_queue, self.append_queue
-        else:
-            q1, q2 = self.append_queue, self.remove_queue
-        while q1.peek() is not None:
-            q2.push(q1.pop())
-
     def enqueue(self, item):
-        self._change_mode()
         self.append_queue.push(item)
 
     def dequeue(self):
-        if self.size() == 0:
-            return None
-        self._change_mode()
+        if self.remove_queue.size() == 0:
+            if self.append_queue.size() == 0:
+                return None
+            while self.append_queue.size() > 0:
+                self.remove_queue.push(self.append_queue.pop())
         return self.remove_queue.pop()
 
     def size(self):
-        return max(self.append_queue.size(), self.remove_queue.size())
+        return self.append_queue.size() + self.remove_queue.size()
 
 
 def rotate_queue(queue, n):
     if queue.size() == 0:
-        raise ValueError("Unable to rotate empty queue")
-    for i in range(n):
+        return
+    for i in range(n % queue.size()):
         queue.enqueue(queue.dequeue())
 
 def reverse_order(queue):
-    rotate_queue(queue, queue.size())
+    if queue.size() == 0:
+        return
+    stack = Stack()
+    while queue.peek() is not None:
+        stack.push(queue.dequeue())
+    while stack.peek() is not None:
+        queue.push(stack.pop())
