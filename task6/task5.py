244ef9dc3d6bdfc671a5eba1ddca74b41a0f7dda
Vycheslav <vycheslav79@gmail.com>
2026-05-31 12:55:18 +0300

    task 6

---
+++ task6/task5.py
@@ -0,0 +1,20 @@
+class Deque:
+    def __init__(self):
+        
+
+    def addFront(self, item):
+        
+
+    def addTail(self, item):
+        
+
+    def removeFront(self):
+        
+        return None
+
+    def removeTail(self):
+        
+        return None
+
+    def size(self):
+        return 0
